f2cbdd81959675e1888dde76900b816921d96b62
gitprofit <liszcz.michal@gmail.com>
2014-01-04 22:28:10 +0100

    added request-specific headers

--- data_storage/include/shared.hrl
+++ data_storage/include/shared.hrl
@@ -7,17 +7,32 @@
 -define(TIMEOUT, 1000).
 
 -record(file, {
-					id,				% string (uuid)
-					name,			% string
-					size,			% in bytes
-					last_access,	% calendar:universal_time()
-					owner_id,		% string (uuid)
-					v_path	= "/"	% string
+					id,					% string (uuid)
+					size,				% in bytes
+					last_access,		% calendar:universal_time()
+					owner_id,			% string (uuid)
+					v_path	= "/empty"	% string
 					}).
 
 -record(request, {
-				  	file_id,	% string (name in create request)
-					user_id,	% string
-					action,		% atom read | write | create | delete
-					options		% reserved for future use
+				  	file_id,			% string (name in create request)
+					user_id,			% string
+					action,				% atom read | write | create | delete | move
+					broadcast = true,	% atom true | false
+					options				% request-specific record
 					}).
+
+-record(create_opts, {
+						v_path = "/empty",	% string
+					  	force_loc = false,	% atom true | false
+						data				% raw data
+						}).
+
+-record(write_opts, {
+						v_path = no_upd,	% undefined = no update
+						data = no_upd		%
+						}).
+
+-record(move_opts, {
+						new_loc		% atom (node)
+  						}).

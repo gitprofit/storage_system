0da2295288c9f69bc41e3865a88f7356a7be9aec
gitprofit <liszcz.michal@gmail.com>
2014-01-05 21:55:19 +0100

    UserId sent in every request

--- data_storage/src/storage_client.erl
+++ data_storage/src/storage_client.erl
@@ -8,18 +8,18 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([create/2,
-		 delete/2,
-		 read/2,
-		 write/3,
+-export([create/3,
+		 delete/3,
+		 read/3,
+		 write/4,
 		 grab_ids/2,
 		 send_request/2]).
 
-create(Node, FileName) ->
+create(Node, FileName, UserId) ->
 	case file:read_file(FileName) of
 		{ ok, RawData } ->
 	send_request(Node, #request{action	= create,
-								user_id	= "usr123",
+								user_id	= UserId,
 								options	= #create_opts{v_path	= FileName,
 													   data 	= RawData}
 								});
@@ -27,23 +27,23 @@ create(Node, FileName) ->
 			io:format("error!~n")
 	end.
 
-delete(Node, FileId) ->
+delete(Node, FileId, UserId) ->
 	send_request(Node, #request{action	= delete,
-								user_id	= "usr123",
+								user_id	= UserId,
 								file_id	= FileId
 							   }).
 
-read(Node, FileId) ->
+read(Node, FileId, UserId) ->
 	send_request(Node, #request{action	= read,
-								user_id	= "usr123",
+								user_id	= UserId,
 								file_id	= FileId
 								}).
 
-write(Node, FileId, FileName) ->
+write(Node, FileId, FileName, UserId) ->
 	case file:read_file(FileName) of
 		{ ok, RawData } ->
 	send_request(Node, #request{action	= write,
-								user_id	= "usr123",
+								user_id	= UserId,
 								file_id = FileId,
 								options	= #write_opts{v_path	= FileName,
 													  data 		= RawData}
